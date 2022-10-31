a2917c4061d2d12e3365632e45a8fdc4d272e24f
Aaron Jones <aaron.p.j@hotmail.com>
2022-11-01 01:54:23 +0200

    deciding what format

--- lem_in_vis.py
+++ lem_in_vis.py
@@ -7,3 +7,9 @@ from time import sleep
 path = os.path.dirname(os.path.abspath(__file__))
 ant_img_path = pygame.image.load(os.path.join(path, 'images', "ant_img.bmp"))
 ant_img = pygame.transform.scale(ant_img_path, (50, 30))
+
+class Lem_in:
+	"""Class for lem_in visualizer"""
+
+	def __init__(self):
+		"""Initialize the visualizer"""
