ff4ec182d91067ca0959a6c08350f58230ac2b79
Aaron Jones <aaron.p.j@hotmail.com>
2022-11-01 01:52:19 +0200

    added image

--- lem_in_vis.py
+++ lem_in_vis.py
@@ -1,3 +1,9 @@
+from subprocess import ABOVE_NORMAL_PRIORITY_CLASS
 import pygame
 import sys
+import os
 from time import sleep
+
+path = os.path.dirname(os.path.abspath(__file__))
+ant_img_path = pygame.image.load(os.path.join(path, 'images', "ant_img.bmp"))
+ant_img = pygame.transform.scale(ant_img_path, (50, 30))
