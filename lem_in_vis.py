ae2e74669e086f7b7b2e020c56ac8beabb27cac5
Aaron Jones <aaron.p.j@hotmail.com>
2022-11-01 01:59:18 +0200

    added title caption

--- lem_in_vis.py
+++ lem_in_vis.py
@@ -1,8 +1,8 @@
-from subprocess import ABOVE_NORMAL_PRIORITY_CLASS
 import pygame
 import sys
 import os
 from time import sleep
+from settings import Settings
 
 path = os.path.dirname(os.path.abspath(__file__))
 ant_img_path = pygame.image.load(os.path.join(path, 'images', "ant_img.bmp"))
@@ -14,3 +14,7 @@ class Lem_in:
 	def __init__(self):
 		"""Initialize the visualizer"""
 		pygame.init()
+		self.settings = Settings()
+
+		self.screen = pygame.display.set_mode()
+		pygame.display.set_caption("Lem_in Visualizer")
