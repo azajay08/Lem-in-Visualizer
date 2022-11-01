00288bce571fbb6dabb60d422756019e3acbce8a
Aaron Jones <aaron.p.j@hotmail.com>
2022-11-01 02:23:52 +0200

    launched first window

--- lem_in_vis.py
+++ lem_in_vis.py
@@ -15,6 +15,46 @@ class Lem_in:
 		"""Initialize the visualizer"""
 		pygame.init()
 		self.settings = Settings()
-
-		self.screen = pygame.display.set_mode()
+		self.screen = pygame.display.set_mode((
+			self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Lem_in Visualizer")
+
+	def run_vis(self):
+		""""""
+		self.screen.fill(self.settings.bg_colour)
+		while True:
+			self._check_events()
+			
+			pygame.display.flip()
+
+	def _check_events(self):
+		"""Function that check events in the program"""
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_q:
+					sys.exit()
+				#check buttons or speed
+				if event.key == pygame.K_1:
+					self.settings.delay = 300
+				if event.key == pygame.K_2:
+					self.settings.delay = 200
+				if event.key == pygame.K_3:
+					self.settings.delay = 100
+				if event.key == pygame.K_4:
+					self.settings.delay = 75
+				if event.key == pygame.K_5:
+					self.settings.delay = 50
+				if event.key == pygame.K_6:
+					self.settings.delay = 25
+				if event.key == pygame.K_7:
+					self.settings.delay = 10
+				if event.key == pygame.K_8:
+					self.settings.delay = 1
+				if event.key == pygame.K_9:
+					self.settings.delay = 0.000001
+
+if __name__ == '__main__':
+	lv = Lem_in()
+	lv.run_vis()
--- settings.py
+++ settings.py
@@ -1,8 +1,17 @@
 import pygame
 import os
 
+black = (0, 0, 0)
+navy = (0, 0, 25)
+
 class Settings:
 	"""Class to store settings variables"""
 
 	def __init__(self):
 		"""Initialize the settings"""
+		# Screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_colour = navy
+
+		self.delay = 200
