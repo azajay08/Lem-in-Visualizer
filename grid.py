2558e5099c37addcb4fc444067231d3e7a19ffbb
Aaron Jones <aaron.p.j@hotmail.com>
2022-11-06 04:16:27 +0200

    tried out midway boxes

--- grid.py
+++ grid.py
@@ -33,7 +33,7 @@ class Grid:
 		y_grid = self.rect.y
 		x_grid = self.rect.x
 		center_xy = self.settings.grid_side / 2
-		center_mid = (self.settings.grid_side / 3) / 2
+		# center_mid = (self.settings.grid_side / 3) / 2
 		self.links = []
 		for room in self.settings.rooms:
 			x_grid += ((room['x'] - self.settings.smallestx) * (self.settings.scale_x))
@@ -50,19 +50,19 @@ class Grid:
 			for link in self.links:
 				if connections['l_1'] in link['name']:
 					link1 = int(link['x_c']), int(link['y_c'])
-					x1 = int(link['x_c'])
-					y1 = int(link['y_c'])
-					print(f"Link1: {link1}")
+					# x1 = int(link['x_c'])
+					# y1 = int(link['y_c'])
+					# print(f"Link1: {link1}")
 			for link in self.links:
 				if connections['l_2'] in link['name']:
 					link2 = int(link['x_c']), int(link['y_c'])
-					x2 = int(link['x_c'])
-					y2 = int(link['y_c'])
-					print(f"Link2: {link2}")
-			coord_x = (x1 + x2) / 2 - center_mid
-			coord_y = (y1 + y2) / 2 - center_mid
+					# x2 = int(link['x_c'])
+					# y2 = int(link['y_c'])
+					# print(f"Link2: {link2}")
+			# coord_x = (x1 + x2) / 2 - center_mid
+			# coord_y = (y1 + y2) / 2 - center_mid
 			pygame.draw.line(self.screen, white, (link1), (link2))
-			self.screen.blit(self.settings.midway, (coord_x, coord_y))
+			# self.screen.blit(self.settings.midway, (coord_x, coord_y))
 		for room in self.settings.rooms:
 			x_grid += ((room['x'] - self.settings.smallestx) * (self.settings.scale_x))
 			y_grid += ((room['y'] - self.settings.smallesty) * (self.settings.scale_y))
