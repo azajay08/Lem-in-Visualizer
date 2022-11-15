f28a30fe6625d2e60116aa088a29c74caf624801
Aaron Jones <aaron.p.j@hotmail.com>
2022-11-15 02:09:41 +0200

    changed colour names

--- ants.py
+++ ants.py
@@ -3,10 +3,10 @@ import os
 import pygame.font
 
 grey = (32, 32, 32)
-p1_old = (139,10,80) #dark pink
-p2_old = (0,139,139) # dark cyan
-p1_new = (255, 0, 127) # pink
-p2_new = (0,238,238) # light cyan
+dark_pink = (139,10,80) #dark pink
+dark_cyan = (0,139,139) # dark cyan
+light_pink = (255, 0, 127) # pink
+light_cyan = (0,238,238) # light cyan
 navy = (0, 0, 25)
 white = (255, 255, 255)
 
